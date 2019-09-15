bd795e4233b7c5c806c6bb64664c5522b43b79ff
Matt Oestreich <matthewpoestreich@gmail.com>
2019-09-15 15:57:36 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const HomeController = require('../controllers/home');
 
-const port = 1337
+const port = 1337;
+const ip = '0.0.0.0';
 
 // set app port
 app.set('port', port);
 
 // set app ip
-app.set('ip', '0.0.0.0');
+app.set('ip', ip);
 
 // set up main routes
 app.use(HomeController)
